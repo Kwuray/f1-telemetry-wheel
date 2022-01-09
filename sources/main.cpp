f9ad67ba5e9b4b598230dd9dd9a7ac8abcfe9674
Kwuray <nicolas@imac-de-nicolas.home>
2022-01-09 03:21:05 +0100

    ...

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,12 +1,23 @@
 #include <QApplication>
 #include <QPushButton>
 
-int main(int argc, char *argv[])
-{
-    QApplication app(argc, argv);
+int main(int argc, char *argv[]) {
+  QApplication app(argc, argv);
 
-    QPushButton bouton("Salut les Zéros, la forme ?");
-    bouton.show();
+
+    // Création d'un widget qui servira de fenêtre
+    QWidget fenetre;
+    fenetre::showMaximized;
+
+    // Création du bouton, ayant pour parent la "fenêtre"
+    QPushButton bouton("Pimp mon bouton !", &fenetre);
+    // Personnalisation du bouton
+    bouton.setFont(QFont("Comic Sans MS", 14));
+    bouton.setCursor(Qt::PointingHandCursor);
+    bouton.setIcon(QIcon("smile.png"));
+
+    // Affichage de la fenêtre
+    fenetre.show();
 
     return app.exec();
 }
